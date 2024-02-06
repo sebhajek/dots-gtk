47b696568ca2519ef3471b0f4fe5288885373f7a
sebhajek <seb.hajek@gmail.com>
2024-02-06 05:47:34 +0100

    gtk

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 sudo mkdir -p /usr/share/themes/oomox-carbonesque;
 sudo cp -ur ~/.setup-dots/dots/wm/gtk/.themes/oomox-carbonesque/* /usr/share/themes/oomox-carbonesque;
 
-sudo mkdir -p /usr/share/icons/oomox-carbonesque
+sudo mkdir -p /usr/share/icons/oomox-carbonesque;
 sudo cp -ur ~/.setup-dots/dots/wm/gtk/.icons/oomox-carbonesque/* /usr/share/icons/oomox-carbonesque;
+
+sudo mkdir -p ~/.config/gtk-3.0;
+cp -ur ~/.setup-dots/dots/wm/gtk/gtk-3.0/* ~/.config/gtk-3.0;
+cp -u ~/.setup-dots/dots/wm/gtk/.gtkrc-2.0 ~/;
