147090fae96386a84de668414673fc87f8849554
sebhajek <seb.hajek@gmail.com>
2024-02-06 05:07:00 +0100

    themes and install script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo mkdir -p /usr/share/themes/oomox-carbonesque;
+sudo cp -ur ~/.setup-dots/dots/wm/gtk/.theme/oomox-carbonesque/* /usr/share/themes/oomox-carbonesque;
+
+sudo mkdir -p /usr/share/icons/oomox-carbonesque
+sudo cp -ur ~/.setup-dots/dots/wm/gtk/.icons/oomox-carbonesque/* /usr/share/icons/oomox-carbonesque;
